7116566e6bcf41c757c514fc7ae309c7c12e9219
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-06-22 06:32:52 +0100

    added my events to the conract

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.28;
 
 contract MyToken {
+    
+    // First I declare my event for the NFT
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
 
 }
