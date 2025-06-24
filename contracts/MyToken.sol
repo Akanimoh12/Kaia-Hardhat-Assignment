f2d7a59d277c27e04687f63394e88f0f27bcfe68
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-06-24 21:06:17 +0100

    Added my constructor

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,17 +2,41 @@
 pragma solidity ^0.8.28;
 
 contract MyToken {
-    
-    // First I declare my event for the NFT
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
 
-    // Next I declare my storages
-    string private _name;
-    string private _symbol;
-    mapping(uint256 => address) private _owners;
-    mapping(address => uint256) private _balance;
-    mapping(uint256 => address) private _tokenApprovals;
-    mapping(address => mapping(address => bool)) private _operatorApprovals;
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 initialSupply) {
+        totalSupply = initialSupply * 10 ** decimals;
+        balances[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+
+
+    
+    // // First I declare my event for the NFT
+    // event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    // event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    // event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    // // Next I declare my storages
+    // string private _name;
+    // string private _symbol;
+    // mapping(uint256 => address) private _owners;
+    // mapping(address => uint256) private _balance;
+    // mapping(uint256 => address) private _tokenApprovals;
+    // mapping(address => mapping(address => bool)) private _operatorApprovals;
 }
